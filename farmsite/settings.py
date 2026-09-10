58a6329dfd7b3a6a6c44efbfa283910f9cde702e
Parth <drfarm7920@gmail.com>
2026-09-10 13:51:04 +0900

    updated settings

--- farmsite/settings.py
+++ farmsite/settings.py
@@ -21,17 +21,23 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'shop',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+ 
+CORS_ALLOWED_ORIGINS = [
+    'https://angan-baari.web.app',
 ]
 
 REST_FRAMEWORK = {
